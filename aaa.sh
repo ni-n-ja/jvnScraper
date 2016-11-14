d56549f8f2c8d022ef5842b7c1bf13cc2757aeec
ikuramoto <atled.ikuramoto@gmail.com>
2016-11-14 11:02:28 +0900

    2桁以上になる月でも正しいクエリパラメータを吐くように

--- aaa.sh
+++ aaa.sh
@@ -5,3 +5,21 @@ node month.js 3 && \
 node month.js 4 && \
 node month.js 5 && \
 node month.js 6
+
+http://jvndb.jvn.jp/search/index.php
+?mode=_vulnerability_search_IA_VulnSearch
+&lang=ja
+&keyword=
+&dateLastPublishedFromYear=2016
+&dateLastPublishedFromMonth=10
+&datePublicFromYear=2016&datePublicFromMonth=10
+&skey=d5
+
+http://jvndb.jvn.jp/search/index.php
+?mode=_vulnerability_search_IA_VulnSearch
+&lang=ja&keyword=
+&dateLastPublishedFromYear=2016
+&dateLastPublishedFromMonth=10
+&datePublicFromYear=2016
+&datePublicFromMonth=10
+&skey=d5&pageNo=2
